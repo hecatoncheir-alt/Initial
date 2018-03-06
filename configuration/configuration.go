59fa22be640dbf8d1e5c5d50aecfe185c62bc69d
Rasarts <rasart.pro@gmail.com>
2018-03-06 22:42:33 +0300

    Update configuration for http and socket servers.

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -90,6 +90,46 @@ func GetConfiguration() (Configuration, error) {
 		configuration.Production.Broker.Port = port
 	}
 
+	// HTTPServer Production
+	productionHTTPServerHostFromEnvironment := os.Getenv("Production-HTTPServer-Host")
+	if productionHTTPServerHostFromEnvironment == "" {
+		configuration.Production.HTTPServer.Host = "localhost"
+	} else {
+		configuration.Production.HTTPServer.Host = productionHTTPServerHostFromEnvironment
+	}
+
+	productionHTTPServerPortFromEnvironment := os.Getenv("Production-HTTPServer-Port")
+	if productionHTTPServerPortFromEnvironment == "" {
+		configuration.Production.HTTPServer.Port = 8080
+	} else {
+		port, err := strconv.Atoi(productionHTTPServerPortFromEnvironment)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		configuration.Production.HTTPServer.Port = port
+	}
+
+	// SocketServer Production
+	productionSocketServerHostFromEnvironment := os.Getenv("Production-SocketServer-Host")
+	if productionSocketServerHostFromEnvironment == "" {
+		configuration.Production.SocketServer.Host = "localhost"
+	} else {
+		configuration.Production.SocketServer.Host = productionSocketServerHostFromEnvironment
+	}
+
+	productionSocketServerPortFromEnvironment := os.Getenv("Production-SocketServer-Port")
+	if productionSocketServerPortFromEnvironment == "" {
+		configuration.Production.SocketServer.Port = 8081
+	} else {
+		port, err := strconv.Atoi(productionSocketServerPortFromEnvironment)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		configuration.Production.SocketServer.Port = port
+	}
+
 	developmentBrokerHostFromEnvironment := os.Getenv("Development-Broker-Host")
 	if developmentBrokerHostFromEnvironment == "" {
 		configuration.Development.Broker.Host = "192.168.99.100"
@@ -107,6 +147,46 @@ func GetConfiguration() (Configuration, error) {
 		}
 
 		configuration.Development.Broker.Port = port
+	}
+
+	// HTTPServer Development
+	developmentHTTPHostFromEnvironment := os.Getenv("Development-HTTPServer-Port")
+	if developmentHTTPHostFromEnvironment == "" {
+		configuration.Development.HTTPServer.Host = "localhost"
+	} else {
+		configuration.Development.HTTPServer.Host = developmentHTTPHostFromEnvironment
+	}
+
+	developmentHTTPServerPortFromEnvironment := os.Getenv("Development-HTTPServer-Port")
+	if developmentHTTPServerPortFromEnvironment == "" {
+		configuration.Development.HTTPServer.Port = 8000
+	} else {
+		port, err := strconv.Atoi(developmentBrokerPortFromEnvironment)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		configuration.Development.HTTPServer.Port = port
+	}
+
+	// SocketServer Development
+	developmentSocketHostFromEnvironment := os.Getenv("Development-SocketServer-Port")
+	if developmentSocketHostFromEnvironment == "" {
+		configuration.Development.SocketServer.Host = "localhost"
+	} else {
+		configuration.Development.SocketServer.Host = developmentSocketHostFromEnvironment
+	}
+
+	developmentSocketServerPortFromEnvironment := os.Getenv("Development-SocketServer-Port")
+	if developmentSocketServerPortFromEnvironment == "" {
+		configuration.Development.SocketServer.Port = 8001
+	} else {
+		port, err := strconv.Atoi(developmentSocketServerPortFromEnvironment)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		configuration.Development.SocketServer.Port = port
 	}
 
 	return configuration, nil
